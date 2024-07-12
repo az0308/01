8d1d992ac27217691c2911462e59ad8bb80b28f6
az0308 <a9291929120@gmail.com>
2024-07-12 10:33:16 +0800

    Update main.jsx

--- Boxing auxiliary scoring/src/main.jsx
+++ Boxing auxiliary scoring/src/main.jsx
@@ -5,8 +5,7 @@ import App from "./App.jsx";
 import { RecoilRoot } from "recoil";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-    <App />
-    </React.StrictMode>,
-  document.getElementById('root')
+    <RecoilRoot>
+        <App />
+    </RecoilRoot>
 );
